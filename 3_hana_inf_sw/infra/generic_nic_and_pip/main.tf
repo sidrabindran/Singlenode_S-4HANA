54d471af6ffba17da463852d0ab99ce4e8f7328b
siddrab <38925735+siddrab@users.noreply.github.com>
2019-07-24 01:32:18 -0700

    Update main.tf

--- 3_hana_inf_sw/infra/generic_nic_and_pip/main.tf
+++ 3_hana_inf_sw/infra/generic_nic_and_pip/main.tf
@@ -21,13 +21,8 @@ resource "azurerm_network_interface" "nic" {
     name      = "${var.name}-nic-configuration"
     subnet_id = "${var.subnet_id}"
 
-    load_balancer_backend_address_pools_ids = ["${var.backend_ip_pool_ids}"]
     private_ip_address_allocation           = "${var.private_ip_address != local.empty_string ? local.static : local.dynamic}"
     private_ip_address                      = "${var.private_ip_address}"
     public_ip_address_id                    = "${azurerm_public_ip.pip.id}"
-  }
-
-  tags {
-    environment = "Terraform SAP HANA deployment"
   }
 }
