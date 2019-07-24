2d2f33ffada4172d98489f127a3a93faff6e3da3
Sid <siddrab@microsoft.com>
2019-07-24 08:17:13 +0000

    hana infrastructure software provisioing

---
+++ 3_hana_inf_sw/infra/hana_infra/terraform.tfvars
@@ -0,0 +1,39 @@
+# Azure region to deploy resource in; please choose the same region as your storage from step 3 (example: "westus2")
+az_region = "eastus2"
+
+# Name of resource group to deploy (example: "demo1")
+az_resource_group = "s4_test_deploy"
+
+# Unique domain name for easy VM access (example: "hana-on-azure1")
+az_domain_name = "hana-on-azure"
+
+# Size of the VM to be deployed (example: "Standard_E8s_v3")
+# For HANA platform edition, a minimum of 32 GB of RAM is recommended
+vm_size = "Standard_E8s_v3"
+
+# Path to the public SSH key to be used for authentication (e.g. "~/.ssh/id_rsa.pub")
+sshkey_path_public = "~/.ssh/id_rsa.pub"
+
+# Path to the corresponding private SSH key (e.g. "~/.ssh/id_rsa")
+sshkey_path_private = "~/.ssh/id_rsa"
+
+# OS user with sudo privileges to be deployed on VM (e.g. "demo")
+vm_user = "demo"
+
+# private IP address
+private_ip_address_hdb = "10.2.0.20"
+
+# Name of the VM
+vm_name = "hanadb"
+
+# SAP system ID (SID) to be used for HANA installation (example: "HN1")
+sap_sid = "HN1"
+
+# Resource group where Virtual Network in deployed
+net_rg_name = "East2_VNET"
+
+# Name of the Virtual network where the VM's will be deployed
+vnet_name = "sapvnet"
+
+# Name of the subnet where SAP will be deployed
+subnet_name = "default"
---
+++ 3_hana_inf_sw/infra/hana_infra/variables.tf
@@ -0,0 +1,73 @@
+variable "allow_ips" {
+  description = "The IP addresses that will be allowed by the nsg"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ansible_playbook_path" {
+  description = "Path from this module to the playbook"
+  default     = "../../ansible/single_node_playbook.yml"
+}
+
+variable "az_domain_name" {
+  description = "Prefix to be used in the domain name"
+}
+
+variable "az_region" {}
+
+variable "az_resource_group" {
+  description = "Which Azure resource group to deploy the HANA setup into.  i.e. <myResourceGroup>"
+}
+
+variable "private_ip_address_hdb" {
+  description = "The desired private IP address of this HANA database.  If it isn't specified, a dynamic IP will be allocated."
+}
+
+variable "public_ip_allocation_type" {
+  description = "Defines whether the IP address is static or dynamic. Options are Static or Dynamic."
+  default     = "Dynamic"
+}
+
+variable "sap_sid" {
+  default = "PV1"
+}
+
+variable "sshkey_path_private" {
+  description = "The path on the local machine to where the private key is"
+}
+
+variable "sshkey_path_public" {
+  description = "The path on the local machine to where the public key is"
+}
+
+variable "storage_disk_sizes_gb" {
+  description = "List disk sizes in GB for all disks this VM will need"
+  default     = [512, 512, 512, 512, 512]
+}
+
+variable "vm_name" {
+  description = "Name of the VM"
+}
+
+variable "use_existing_nsg" {
+  default = false
+}
+
+variable "vm_size" {
+  default = "Standard_E8s_v3"
+}
+
+variable "vm_user" {
+  description = "The username of your HANA database VM."
+}
+
+variable "net_rg_name" {
+  description = "Resource group where Virtual Network in deployed"
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual network where the VM's will be deployed"
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet where SAP will be deployed"
+}
